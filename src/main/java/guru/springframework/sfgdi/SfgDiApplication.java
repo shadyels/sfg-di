49abff21098ad7634c02f59f936a113d1f2b2d73
Shadi <shadi@noumenadigital.com>
2020-10-31 22:04:02 +0100

    all the way to profile

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,38 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.I18nController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInejctedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
+		System.out.println("------ Primary");
+		MyController myController = (MyController) ctx.getBean("myController");
+		System.out.println(myController.sayHello());
+
+		System.out.println("------ Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------ Setter");
+		SetterInejctedController setterInejctedController = (SetterInejctedController) ctx.getBean("setterInejctedController");
+		System.out.println(setterInejctedController.getGreeting());
+
+		System.out.println("------ Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+}
