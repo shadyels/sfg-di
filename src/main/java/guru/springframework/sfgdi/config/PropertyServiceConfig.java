1e398dbf174cfa0c5bf5f63bbcfd6294f385ed78
Shadi <shadi@noumenadigital.com>
2020-11-27 10:23:24 +0100

    application props also in yaml

--- src/main/java/guru/springframework/sfgdi/config/PropertyServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyServiceConfig.java
@@ -1,16 +1,21 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
+import guru.springframework.sfgdi.examplebeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//When not using Application.properties I can use this to add ext prop files. Application.properties is wired automatically.
+//@PropertySource({
+//    "classpath:datasource.properties",
+//    "classpath:jim.properties"
+//  })
 public class PropertyServiceConfig {
-
     @Value("${guru.username}")
     String user;
 
@@ -20,6 +25,15 @@ public class PropertyServiceConfig {
     @Value("${guru.dburl}")
     String url;
 
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -28,6 +42,16 @@ public class PropertyServiceConfig {
         fakeDataSource.setUrl(url);
 
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUser(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+
+        return fakeJmsBroker;
     }
 
     @Bean
